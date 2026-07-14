3d4995580d124eabafc03811f3777ee3f1f1fc52
Gourav <gouravasija1604@gmail.com>
2026-07-14 13:27:51 +0530

    Fetch API data and implement weatherfetch function

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //API configuration
 const BASE_URL='https://api.openweathermap.org/data/2.5'
  console.log("WeatherSphere Loaded Successfully")
- fetch(`${BASE_URL}/weather?q=London&appid=${API_KEY}`)
- .then(response=>response.json())
- .then(data=>console.log(data))
- .catch(error=>console.log(error))
  //DOM references
   const searchInput=document.getElementById("input-text")
   const searchBtn=document.getElementById("searchBtn")
   const locateBtn=document.getElementById("locationBtn")
   const recentList=document.getElementById("recent-list")
+  const cityEl=document.getElementById("city")
   const countryEl=document.getElementById("country")
   const dayEl=document.getElementById("day")
   const dateTimeEl=document.getElementById("date-time")
@@ -27,31 +24,69 @@ const BASE_URL='https://api.openweathermap.org/data/2.5'
   const aiEl=document.getElementById("advice")
   const updated=document.getElementById("update")
   const errorEl=document.querySelector(".error")
-  const loadingEl=document.querySelector("spinner")
+  const loadingEl=document.querySelector(".loading")
   const celsiusBtn=document.getElementById("celsius")
   const fahrenBtn=document.getElementById("fera")
+  //Search-section 
 searchBtn.addEventListener('click',()=>{
-  if(searchInput.value.trim()===""){
+    let city= searchInput.value.trim()
+    console.log(city)
+  if(city===""){
   errorEl.style.display="block";
   setTimeout(()=>{
     errorEl.style.display="none"
 },3000)
-  }else{
-
+return
   }
+  fetchWeather(city)
+  searchInput.value=""
 })
 searchInput.addEventListener('keydown',(e)=>{
+    let city= searchInput.value.trim()
     if(e.key==="Enter"){
-  if(searchInput.value.trim()===""){
+  if(city===""){
   errorEl.style.display="block";
   setTimeout(()=>{
     errorEl.style.display="none"
 },3000)
-  }else{
-
+return;
   }
+  fetchWeather(city)
+  
+  searchInput.value=""
 }
 })
+async function fetchWeather(city){
+    loadingEl.style.display="block"
+    errorEl.style.display="none"
+    try{
+     let response=await fetch(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}`)
+     console.log(response.ok)
+     if(!response.ok){
+      throw  new err
+     }
+     let data= await response.json()
+      await console.log(data)
+      displayWeather(data)
+    }
+    catch(err){
+         errorEl.style.display="block"
+        setTimeout(()=>{
+             errorEl.style.display="none"
+        },3000)
+    } 
+    finally {
+        loadingEl.style.display="none"   
+    }
+} 
+ function displayWeather(data){
+    cityEl.textContent=data.name  
+    countryName=data.sys.country 
+    const regionNames=new Intl.DisplayNames(['en'],{type:'region'}) 
+    const completeCountryName=regionNames.of(countryName)
+    countryEl.textContent=completeCountryName
+ }
+
 
 
 
